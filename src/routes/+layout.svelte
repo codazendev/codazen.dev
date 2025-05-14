020828f16e9ca41c13b16e65ee447c7bc68115ab
Jeremy King <codazendev@gmail.com>
2025-05-14 18:28:19 -0400

    reconstructed svelte project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<!-- +layout.svelte -->
+<script>
+    let { children } = $props();
+</script>
+
+<header>Hi, I'm a header</header>
+
+<main>
+    {@render children()}
+</main>
+
+<footer>Hello, I'm the footer.</footer>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,4 @@
+<!-- contact/+page.svelte -->
+<h1>Get in touch</h1>
+
+<p><a href="mailto:jeremykingdev@gmail.com">Email me!</a></p>
